6caeac4307ca51e73af86dc78a4cf1be05df3f3d
Alex Ianus <hire@alexianus.com>
2022-06-12 21:03:54 -0700

    Install .bash_aliases in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 mv /home/gitpod/.gitconfig /home/gitpod/.gitconfig.bak
 ln -s /home/gitpod/.dotfiles/.gitconfig /home/gitpod/.gitconfig
+ln -s /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
 
