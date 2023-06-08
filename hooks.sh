22f7e7c8339e42bf85386fc06a81b6d497d785ce
Alex Ianus <hire@alexian.us>
2023-06-08 16:36:52 -0700

    Comment out extensions, use vscode sync

--- hooks.sh
+++ hooks.sh
@@ -2,20 +2,20 @@
 
 gp await-port 23000
 
-_exts=(
-  ms-python.python
-  kylepaulsen.stretchy-spaces
-  redhat.vscode-xml
-  redhat.vscode-yaml
-  esbenp.prettier-vscode
-  ms-kubernetes-tools.vscode-kubernetes-tools
-  reduckted.vscode-gitweblinks
-  eamodio.gitlens
-  huizhou.githd
-  ms-azuretools.vscode-docker
-  hashicorp.terraform
-  github.copilot
-)
-for _ext in "${_exts[@]}"; do {
-   code --install-extension "$_ext";
-} done
+# _exts=(
+#   ms-python.python
+#   kylepaulsen.stretchy-spaces
+#   redhat.vscode-xml
+#   redhat.vscode-yaml
+#   esbenp.prettier-vscode
+#   ms-kubernetes-tools.vscode-kubernetes-tools
+#   reduckted.vscode-gitweblinks
+#   eamodio.gitlens
+#   huizhou.githd
+#   ms-azuretools.vscode-docker
+#   hashicorp.terraform
+#   github.copilot
+# )
+# for _ext in "${_exts[@]}"; do {
+#    code --install-extension "$_ext";
+# } done
