3adeaffe2d162de9c732a54f1004fc645c6e74b3
Alex Ianus <hire@alexian.us>
2023-06-08 13:33:16 -0700

    Fix admin superpowers

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ ln -s /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
 cp /home/gitpod/.dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
 
 # Use Administrator super-powers
-awk '{gsub(/dev-temp-privileged/,"default")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config
-awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config
+awk '{gsub(/dev-temp-privileged/,"default")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
+awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
