e4fefc82803b2d2ba327dc2e477bf05ac47bd54f
Alex Ianus <hire@alexian.us>
2023-06-08 13:27:05 -0700

    Use administrator access by default

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ ln -s /home/gitpod/.dotfiles/.gitconfig /home/gitpod/.gitconfig
 ln -s /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
 cp /home/gitpod/.dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
 
+# Use Administrator super-powers
+awk '{gsub(/dev-temp-privileged/,"default")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config
+awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config
