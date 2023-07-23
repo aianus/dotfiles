fc5178e3cd6c0437885acd78eea0ad6f01b0f2e3
Alex Ianus <hire@alexian.us>
2023-07-23 11:00:06 -0700

    Try with absolute path to npm

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ awk '{gsub(/dev-temp-privileged/,"default")};1' /home/gitpod/.aws/config > /home
 awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
 
 # Install AI shell helper
-npm install -g --registry=https://nexus.internaldns-snaptravel.com/repository/npm/ @builder.io/ai-shell
+/home/gitpod/.nvm/versions/node/v16.17.0/bin/npm install -g --registry=https://nexus.internaldns-snaptravel.com/repository/npm/ @builder.io/ai-shell
 ai config set OPENAI_KEY=$OPENAI_API_KEY
