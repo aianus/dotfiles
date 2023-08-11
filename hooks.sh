5438aa7fa7d33aa79d7c395bc7515a99744e228e
Alex Ianus <hire@alexian.us>
2023-08-11 15:23:51 -0700

    Install my extensions since settings sync broke

--- hooks.sh
+++ hooks.sh
@@ -6,20 +6,21 @@ gp await-port 23000
 npm install -g --registry=https://nexus.internaldns-snaptravel.com/repository/npm/ @builder.io/ai-shell
 ai config set OPENAI_KEY=$OPENAI_API_KEY
 
-# _exts=(
-#   ms-python.python
-#   kylepaulsen.stretchy-spaces
-#   redhat.vscode-xml
-#   redhat.vscode-yaml
-#   esbenp.prettier-vscode
-#   ms-kubernetes-tools.vscode-kubernetes-tools
-#   reduckted.vscode-gitweblinks
-#   eamodio.gitlens
-#   huizhou.githd
-#   ms-azuretools.vscode-docker
-#   hashicorp.terraform
-#   github.copilot
-# )
-# for _ext in "${_exts[@]}"; do {
-#    code --install-extension "$_ext";
-# } done
+_exts=(
+  ms-python.python
+  kylepaulsen.stretchy-spaces
+  redhat.vscode-xml
+  redhat.vscode-yaml
+  esbenp.prettier-vscode
+  ms-kubernetes-tools.vscode-kubernetes-tools
+  reduckted.vscode-gitweblinks
+  eamodio.gitlens
+  huizhou.githd
+  ms-azuretools.vscode-docker
+  hashicorp.terraform
+  github.copilot
+  timkmecl.chatgpt
+)
+for _ext in "${_exts[@]}"; do {
+   code --install-extension "$_ext";
+} done
