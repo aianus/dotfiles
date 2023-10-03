fc5e8c4e3d919d7aced030cf0acabfed131c3636
Alex Ianus <hire@alexian.us>
2023-10-03 22:49:37 +0200

    Try again with coder

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 if [ -n "$CODER" ]; then
   mv /home/gitpod/.gitconfig /home/gitpod/.gitconfig.bak
-  ln -s /home/gitpod/.dotfiles/.gitconfig /home/gitpod/.gitconfig
-  ln -s /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
-  cp /home/gitpod/.dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
+  ln -s /home/coder/.config/coderv2/dotfiles/.gitconfig /home/gitpod/.gitconfig
+  ln -s /home/coder/.config/coderv2/dotfiles/.bash_aliases /home/gitpod/.bash_aliases
+  cp /home/coder/.config/coderv2/dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
 
   # Use Administrator super-powers
   awk '{gsub(/dev-temp-privileged/,"default")};1' /home/coder/.aws/config > /home/coder/.aws/config.tmp && mv /home/coder/.aws/config.tmp /home/coder/.aws/config
