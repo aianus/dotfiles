1be4aa49fe6eb605a9e7f19dd502a933e073c63a
Alex Ianus <hire@alexian.us>
2023-10-03 22:38:18 +0200

    Make it work with coder too

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-mv /home/gitpod/.gitconfig /home/gitpod/.gitconfig.bak
-ln -s /home/gitpod/.dotfiles/.gitconfig /home/gitpod/.gitconfig
-ln -s /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
-cp /home/gitpod/.dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
+if [ -n "$CODER" ]; then
+  mv /home/gitpod/.gitconfig /home/gitpod/.gitconfig.bak
+  ln -s /home/gitpod/.dotfiles/.gitconfig /home/gitpod/.gitconfig
+  ln -s /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
+  cp /home/gitpod/.dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
 
-# Use Administrator super-powers
-awk '{gsub(/dev-temp-privileged/,"default")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
-awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
+  # Use Administrator super-powers
+  awk '{gsub(/dev-temp-privileged/,"default")};1' /home/coder/.aws/config > /home/coder/.aws/config.tmp && mv /home/coder/.aws/config.tmp /home/coder/.aws/config
+  awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/coder/.aws/config > /home/coder/.aws/config.tmp && mv /home/coder/.aws/config.tmp /home/coder/.aws/config
+elif [ -n "$GITPOD" ]; then
+  mv /home/gitpod/.gitconfig /home/gitpod/.gitconfig.bak
+  ln -s /home/gitpod/.dotfiles/.gitconfig /home/gitpod/.gitconfig
+  ln -s /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
+  cp /home/gitpod/.dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
+
+  # Use Administrator super-powers
+  awk '{gsub(/dev-temp-privileged/,"default")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
+  awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
+else
+  echo "Not running in Coder or Gitpod"
+fi
