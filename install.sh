438036537802bc087d116926162b7aaa53f50325
Alex Ianus <hire@alexianus.com>
2025-06-10 13:13:04 -0700

    Copy instead of linking gitconfig and aliases

--- install.sh
+++ install.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
 if [ -n "$CODER" ]; then
-  # mv /home/coder/.gitconfig /home/coder/.gitconfig.bak
-  ln -s /home/coder/.config/coderv2/dotfiles/.gitconfig /home/coder/.gitconfig
-  ln -s /home/coder/.config/coderv2/dotfiles/.bash_aliases /home/coder/.bash_aliases
-  # cp /home/coder/.config/coderv2/dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
+  cp /home/coder/.config/coderv2/dotfiles/.gitconfig /home/coder/.gitconfig
+  cp /home/coder/.config/coderv2/dotfiles/.bash_aliases /home/coder/.bash_aliases
 
   # Use Administrator super-powers
   awk '{gsub(/dev-temp-privileged/,"default")};1' /home/coder/.aws/config > /home/coder/.aws/config.tmp && mv /home/coder/.aws/config.tmp /home/coder/.aws/config
   awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/coder/.aws/config > /home/coder/.aws/config.tmp && mv /home/coder/.aws/config.tmp /home/coder/.aws/config
-elif [ -n "$GITPOD_REPO_PATH" ]; then
-  mv /home/gitpod/.gitconfig /home/gitpod/.gitconfig.bak
-  ln -s /home/gitpod/.dotfiles/.gitconfig /home/gitpod/.gitconfig
-  ln -s /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
-  cp /home/gitpod/.dotfiles/hooks.sh /workspace/remote-dev/hooks.sh
-
-  # Use Administrator super-powers
-  awk '{gsub(/dev-temp-privileged/,"default")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
-  awk '{gsub(/DeveloperAccess/,"AdministratorAccess")};1' /home/gitpod/.aws/config > /home/gitpod/.aws/config.tmp && mv /home/gitpod/.aws/config.tmp /home/gitpod/.aws/config
 else
-  echo "Not running in Coder or Gitpod"
+  echo "Not running in Coder"
 fi
 
 # Install AI shell helper
